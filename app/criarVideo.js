a052f3903b6718fdd4d30b08076aed4623aef3f7
israel <israelwendellc@gmail.com>
2023-09-13 20:31:11 -0300

    metodo post

---
+++ app/criarVideo.js
@@ -0,0 +1,19 @@
+import { conectaApi } from "./conexaAPi.js";
+
+const formulario = document.querySelector ('[data-formulario]');
+
+formulario.addEventListener('submit', evento => {adicionarVideo (evento)});
+
+async function adicionarVideo (evento) {
+evento.preventDefault ();
+const url = document.querySelector ('[data-url]').value;
+const titulo = document.querySelector ('[data-titulo]').value;
+const imagem = document.querySelector ('[data-imagem]').value;
+const descricao = Math.floor (Math.random () *10).toString () ;
+console.log (titulo)
+
+await conectaApi.criaVideos (titulo,descricao,url,imagem); 
+
+window.location.href = "../pages/envio-concluido.html";
+ 
+}
